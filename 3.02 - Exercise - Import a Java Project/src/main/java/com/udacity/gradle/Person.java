ac9084b644cab69bd4b2032984c65d601ec299d7
Jeremy Silver <jeremy@udacity.com>
2015-03-31 14:51:10 -0700

    Add initial lesson 3 content

---
+++ 3.02 - Exercise - Import a Java Project/src/main/java/com/udacity/gradle/Person.java
@@ -0,0 +1,20 @@
+package com.udacity.gradle;
+
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello from Java");
+    }
+
+
+
+}
