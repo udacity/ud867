3961b885cb17da39031af51c64d2078c6265b92e
Jeremy Silver <jeremy@udacity.com>
2015-04-19 16:32:39 -0700

    Finish 3.02

---
+++ 3.02-Exercise-ImportAJavaProject/src/main/java/com/udacity/gradle/JokeTeller.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle;
+
+
+public class JokeTeller {
+
+    public static void main(String[] args){
+        System.out.println("Java. Write once, debug everywere.");
+    }
+}
