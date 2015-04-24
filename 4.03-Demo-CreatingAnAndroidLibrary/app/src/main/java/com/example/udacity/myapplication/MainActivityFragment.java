7b06519bcda45e9ef2b5185291b5ac9ddd67b950
Jeremy Silver <jeremy@udacity.com>
2015-04-24 14:02:15 -0700

    Add initial version of 4.03

---
+++ 4.03-Demo-CreatingAnAndroidLibrary/app/src/main/java/com/example/udacity/myapplication/MainActivityFragment.java
@@ -0,0 +1,29 @@
+package com.example.udacity.myapplication;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.udacity.mylibrary.ImageActivity;
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void launchLibraryActivity(View view){
+        Intent myIntent =   new Intent(getActivity(), ImageActivity.class);
+        startActivity(myIntent);
+    }
+}
