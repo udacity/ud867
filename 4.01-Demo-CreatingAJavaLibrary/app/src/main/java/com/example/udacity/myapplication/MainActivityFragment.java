010f7bd4fdc52a100ae26b7fd2caaec1e2de7733
Jeremy Silver <jeremy@udacity.com>
2015-04-24 09:35:16 -0700

    Add initial 4.01 demo

---
+++ 4.01-Demo-CreatingAJavaLibrary/app/src/main/java/com/example/udacity/myapplication/MainActivityFragment.java
@@ -0,0 +1,32 @@
+package com.example.udacity.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.udacity.gradle.jokes.Joker;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.textView);
+        Joker myJoker = new Joker();
+        textView.setText(myJoker.getJoke());
+
+
+
+        return rootView;
+    }
+}
---
+++ 4.01-Demo-CreatingAJavaLibrary/javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.jokes;
+
+public class Joker {
+    public String getJoke(){
+        return "This is totally a funny joke";
+    }
+}
