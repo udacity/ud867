4ecb45f1980dc5c6c5d921064522e20b38d455bf
Jeremy Silver <jeremy@udacity.com>
2015-05-06 14:26:20 -0700

    Add initial 4.04

---
+++ 4.04-Exercise-CreateAnAndroidLibrary/solution/jokedisplay/src/main/java/com/udacity/gradle/jokedisplay/JokeActivity.java
@@ -0,0 +1,41 @@
+package com.udacity.gradle.jokedisplay;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class JokeActivity extends ActionBarActivity {
+
+    public static String JOKE_KEY = "Joke key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ 4.04-Exercise-CreateAnAndroidLibrary/solution/jokesource/src/main/java/com/udacity/gradle/jokesource/JokeSource.java
@@ -0,0 +1,8 @@
+package com.udacity.gradle.jokesource;
+
+public class JokeSource {
+
+    public String getJoke(){
+        return "This is a joke from a Java Library";
+    }
+}
