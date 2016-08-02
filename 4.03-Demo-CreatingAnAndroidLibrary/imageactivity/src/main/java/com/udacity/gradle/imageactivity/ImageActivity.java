8142912af4f3549021c54d7d556690c2da13cdc5
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-08-03 00:01:41 +0530

    remove launchLibraryActivity from imageactivity module

--- 4.03-Demo-CreatingAnAndroidLibrary/imageactivity/src/main/java/com/udacity/gradle/imageactivity/ImageActivity.java
+++ 4.03-Demo-CreatingAnAndroidLibrary/imageactivity/src/main/java/com/udacity/gradle/imageactivity/ImageActivity.java
@@ -1,11 +1,9 @@
 package com.udacity.gradle.imageactivity;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
 public class ImageActivity extends ActionBarActivity {
@@ -37,10 +35,5 @@ public class ImageActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void launchLibraryActivity(View view){
-        Intent myIntent = new Intent(this, ImageActivity.class);
-        startActivity(myIntent);
     }
 }
