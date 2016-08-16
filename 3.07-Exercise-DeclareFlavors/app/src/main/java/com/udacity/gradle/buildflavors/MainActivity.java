1454d9a71ada33814ced466155a03c8d3e687ef7
Jeremy Silver <jeremy@udacity.com>
2016-08-16 16:30:02 -0700

    Clean up 3.07

--- 3.07-Exercise-DeclareFlavors/app/src/main/java/com/udacity/gradle/buildflavors/MainActivity.java
+++ 3.07-Exercise-DeclareFlavors/app/src/main/java/com/udacity/gradle/buildflavors/MainActivity.java
@@ -1,8 +1,8 @@
 package com.udacity.gradle.buildflavors;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
