a7a723b1dd396ef5bde7d2f121a7fc03242f7db4
Jeremy Silver <jeremy@udacity.com>
2016-08-17 09:22:20 -0700

    Clean up 4.02

--- 4.02-Exercise-CreateAJavaLibrary/solution/app/src/main/java/com/example/udacity/myapplication/MainActivityFragment.java
+++ 4.02-Exercise-CreateAJavaLibrary/solution/app/src/main/java/com/example/udacity/myapplication/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.example.udacity.myapplication;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,8 +33,6 @@ public class MainActivityFragment extends Fragment {
         String handcraftedJoke = myJokeSmith.tellAHandCraftedJoke();
         TextView handcraftedJokeTextView = (TextView) rootView.findViewById(R.id.handcraftedJokeTextView);
         handcraftedJokeTextView.setText(handcraftedJoke);
-
-
 
 
         return rootView;
