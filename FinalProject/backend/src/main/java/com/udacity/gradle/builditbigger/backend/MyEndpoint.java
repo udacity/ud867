e0246c7b5037bb54a8ec6642dbc5923372fd89ce
Jose Nieto <dnbit@users.noreply.github.com>
2018-01-03 22:52:21 +0000

    Update GCE to V2

--- FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,9 +1,3 @@
-/*
-   For step-by-step instructions on connecting your Android application to this backend module,
-   see "App Engine Java Endpoints Module" template documentation at
-   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
-*/
-
 package com.udacity.gradle.builditbigger.backend;
 
 import com.google.api.server.spi.config.Api;
