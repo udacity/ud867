f7508567cd9128b843a66a83d2898c288f050f1a
EberthFelipe <eberthfelipe@gmail.com>
2019-03-01 01:08:04 -0400

    Step 5: Create different configs for paid and free flavors

--- FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragmentFree.java
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragmentFree.java
@@ -7,15 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragmentFree extends Fragment {
 
     private LinearLayout mViewLoading;
     private LinearLayout mViewNoInternet;
 
-    public MainActivityFragment() {
+    public MainActivityFragmentFree() {
     }
 
     @Override
@@ -25,6 +29,15 @@ public class MainActivityFragment extends Fragment {
         mViewLoading = root.findViewById(R.id.in_loading);
         mViewNoInternet = root.findViewById(R.id.in_no_internet);
 
+
+        AdView mAdView = root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
         return root;
     }
 
---
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFree.java
@@ -0,0 +1,108 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.udacity.gradle.builditbigger.idlingResource.LoadingJokerResource;
+import com.udacity.gradle.builditbigger.presenter.JokerPresenterImpl;
+
+import udacity.android.com.androidlibrary.AndroidLibraryMainActivity;
+
+public class MainActivityFree extends AppCompatActivity implements JokerEndPointResult{
+
+    private JokerPresenterImpl mJokerPresenterImpl;
+
+    //Testing variable
+    @Nullable
+    private LoadingJokerResource mIdlingResource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mJokerPresenterImpl = new JokerPresenterImpl(this, this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        mJokerPresenterImpl.fetchJokeFromApi();
+        if (mIdlingResource != null) {
+            mIdlingResource.setIdleState(false);
+        }
+    }
+
+    @Override
+    public void retrieveJoker(String joke) {
+        if (mIdlingResource != null) {
+            mIdlingResource.setIdleState(true);
+        }
+        Intent intent = new Intent(this, AndroidLibraryMainActivity.class);
+        intent.putExtra(AndroidLibraryMainActivity.JOKER_TEXT, joke);
+        startActivity(intent);
+    }
+
+    @Override
+    public void showProgress(boolean show) {
+        MainActivityFragmentFree mainActivityFragment = (MainActivityFragmentFree) getSupportFragmentManager().getFragments().get(0);
+        mainActivityFragment.showProgress(show);
+    }
+
+    @Override
+    public void showNoInternetConnection() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mIdlingResource != null) {
+                    mIdlingResource.setIdleState(true);
+                }
+                MainActivityFragmentFree mainActivityFragment = (MainActivityFragmentFree) getSupportFragmentManager().getFragments().get(0);
+                mainActivityFragment.showNoInternetConnection();
+            }
+        });
+    }
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        return getLoadingRecipesResource();
+    }
+
+    @VisibleForTesting
+    private LoadingJokerResource getLoadingRecipesResource(){
+        if (mIdlingResource == null) {
+            mIdlingResource = new LoadingJokerResource();
+        }
+        return mIdlingResource;
+    }
+
+}
