ae291d035bfd00011f7a66a6b3d80511952ee1d9
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-09-25 17:31:14 +0200

    modified method name

--- FinalProject/jokeslib/src/main/java/com/example/jokeslib/JokeProvider.java
+++ FinalProject/jokeslib/src/main/java/com/example/jokeslib/JokeProvider.java
@@ -1,7 +1,7 @@
 package com.example.jokeslib;
 
 public class JokeProvider {
-    public String getJoker(){
+    public String getJoke(){
         return "What do you call a fish without eyes? \nFsh.";
     }
 }
