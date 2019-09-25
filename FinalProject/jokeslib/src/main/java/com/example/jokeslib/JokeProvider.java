e57a484c4867490f80291a1b6b5ff8578fb7b423
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-09-25 17:30:52 +0200

    changed joke

--- FinalProject/jokeslib/src/main/java/com/example/jokeslib/JokeProvider.java
+++ FinalProject/jokeslib/src/main/java/com/example/jokeslib/JokeProvider.java
@@ -2,6 +2,6 @@ package com.example.jokeslib;
 
 public class JokeProvider {
     public String getJoker(){
-        return "What do you call a fish without eyes? \n Fsh.";
+        return "What do you call a fish without eyes? \nFsh.";
     }
 }
