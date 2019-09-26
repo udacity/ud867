27e9fa1db116e1fed251b86185db2385263552c3
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-09-26 20:27:19 +0200

    added free and paid versions of the app

--- FinalProject/app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -1,10 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
